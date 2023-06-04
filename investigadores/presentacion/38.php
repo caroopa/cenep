8298efbc57621491b0973686c2ad7068fa970ba7
Carolina Denisse Paleari <palearicaro@gmail.com>
2023-06-04 00:12:15 -0300

    Agrego imagen Comelatto.

--- investigadores/presentacion/38.php
+++ investigadores/presentacion/38.php
@@ -32,7 +32,10 @@
                     </div>
                     <div class="texto">
                         <p><a href="cv/CV_Comelatto.pdf" target="_blank">cv completo | </a><a href="mailto:pcomelatto@cenep.org.ar">pcomelatto@cenep.org.ar</a></p>
-                        <p>Es Doctor en Demografía y Master en Economía y Demografía, por la Universidad de California, Berkeley. Licenciado en Economía (Universidad de Buenos Aires). Profesor Regular de la Universidad de Buenos Aires (Facultad de Ciencias Económicas). Ha desempeñado puestos docentes en grado y postgrado en la Universidad Nacional de Luján (Maestría en Demografía Social), Universidad Católica Argentina (Maestría en Sociología) y IES Abroad Buenos Aires. Es miembro del <em>National Transfer Accounts Project</em>. Se ha desempeñado como consultor para Banco Mundial, Comisión Económica para América Latina-CEPAL, Banco Interamericano de Desarrollo, entre otros. Sus áreas de interés incluyen el impacto económico de los procesos demográficos, principalmente envejecimiento poblacional y migraciones y su impacto en las transferencias intergeneracionales, el mercado de trabajo y las finanzas públicas.</p>
+                        <div class="container-imagen">
+                            <img src="img/Pablo Comelatto.jpg" alt="Pablo Comelatto" class="img-fluid">
+                            <p>Es Doctor en Demografía y Master en Economía y Demografía, por la Universidad de California, Berkeley. Licenciado en Economía (Universidad de Buenos Aires). Profesor Regular de la Universidad de Buenos Aires (Facultad de Ciencias Económicas). Ha desempeñado puestos docentes en grado y postgrado en la Universidad Nacional de Luján (Maestría en Demografía Social), Universidad Católica Argentina (Maestría en Sociología) y IES Abroad Buenos Aires. Es miembro del <em>National Transfer Accounts Project</em>. Se ha desempeñado como consultor para Banco Mundial, Comisión Económica para América Latina-CEPAL, Banco Interamericano de Desarrollo, entre otros. Sus áreas de interés incluyen el impacto económico de los procesos demográficos, principalmente envejecimiento poblacional y migraciones y su impacto en las transferencias intergeneracionales, el mercado de trabajo y las finanzas públicas.</p>
+                        </div>
                     </div>
                 </div>
             </div>
